747aa53d404bf0252d9d7374924c3edf2f6d358b
soupisnotveryepic1 <a1851275@student.adelaide.edu.au>
2022-09-06 21:25:14 +0930

    try 1

--- nibbles-04/src/test.cpp
+++ nibbles-04/src/test.cpp
@@ -1,19 +1,45 @@
-#include <SFML/Window.hpp>
-
+#include <SFML/Graphics.hpp>
+using namespace sf;
 int main()
 {
-    sf::Window window(sf::VideoMode(800, 600), "My window");
+    RenderWindow window(VideoMode(800, 800), "Project");
+    CircleShape shape(30);
+    shape.setFillColor(Color::Red);
+    shape.setOrigin(Vector2f(0,0));
+    shape.setPosition(Vector2f(400,400));
 
     // run the program as long as the window is open
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
+        window.clear();
+        window.draw(shape);
+        window.display();
+        Event event;
         while (window.pollEvent(event))
         {
             // "close requested" event: we close the window
-            if (event.type == sf::Event::Closed)
+            if (event.type == Event::Closed)
                 window.close();
+            if (Keyboard::isKeyPressed(Keyboard::Right)){
+                Vector2f pos = shape.getPosition();
+                pos.x = pos.x + 2;
+                shape.setPosition(pos);
+            }
+            if (Keyboard::isKeyPressed(Keyboard::Left)){
+                Vector2f pos = shape.getPosition();
+                pos.x = pos.x - 2;
+                shape.setPosition(pos);
+            }
+            if (Keyboard::isKeyPressed(Keyboard::Up)){
+                Vector2f pos = shape.getPosition();
+                pos.y = pos.y - 2;
+                shape.setPosition(pos);
+            }
+            if (Keyboard::isKeyPressed(Keyboard::Down)){
+                Vector2f pos = shape.getPosition();
+                pos.y = pos.y + 2;
+                shape.setPosition(pos);
+            }
         }
     }
 
