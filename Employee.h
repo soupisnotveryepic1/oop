5c95eb89bfa87962b68dde591f3ebb98961f7238
Michael Lee <a1851275@student.adelaide.edu.au>
2022-10-26 13:20:52 +1030

    1.1

--- Employee.h
+++ Employee.h
@@ -12,5 +12,8 @@ public:
     void takeABreak(int mins);
     virtual void work(int mins) = 0;
     virtual float pay() = 0;
-
+    int get_payRate(){return payRate;}
+    float get_energyLevel(){return energyLevel;}
+    void set_payRate(int n_rate){payRate = n_rate;}
+    void set_energyLevel(float n_energy){energyLevel = n_energy;}
 };
